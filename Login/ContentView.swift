dc85cb690965def349c1a245c4e16a2f53365280
iamkimbert <iamkimbert@gmail.com>
2022-09-15 13:21:00 -0400

    Initial Commit

---
+++ Login/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Login
+//
+//  Created by Kimberly Townsend on 9/15/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Login/LoginApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginApp.swift
+//  Login
+//
+//  Created by Kimberly Townsend on 9/15/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
